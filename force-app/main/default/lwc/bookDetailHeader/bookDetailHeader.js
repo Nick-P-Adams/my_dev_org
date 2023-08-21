d033a56db18a6479a313cdb193f8bf74a3936372
Thedon989 <nick.patrick.adams@gmail.com>
2023-08-21 14:37:55 -0500

    Dynamically updating the bookDetailHeader with book object data.

--- force-app/main/default/lwc/bookDetailHeader/bookDetailHeader.js
+++ force-app/main/default/lwc/bookDetailHeader/bookDetailHeader.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 
-export default class BookDetailHeader extends LightningElement {}
+
+const fields = [
+    'Book.Name'
+];
+export default class BookDetailHeader extends LightningElement {}    @wire(getRecord, { recordId: '$recordId', fields})
+    book;
+    get name() {
+        return this.contact.data.fields.Name.value;
+    }
